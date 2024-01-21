845d9f4f737c0eb6f01bcefacc4da992d4b52fdf
Tuan Ngo <cjhobabe@gmail.com>
2024-01-21 14:22:05 +0700

    Release v6

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Hello, world! \n This is flask app(v-2.0.11). [release is updated v5]\n"
+MESSAGE = "Hello, world! \n This is flask app(v-2.0.11). [release is updated v6]\n"
 
 app = Flask(__name__)
 
